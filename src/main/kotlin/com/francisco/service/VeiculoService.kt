146d2407b9dd7b872a3cea1dfdb3fcef8d25fabe
franciscoguinarte <francisco.guinarte@outlook.com.br>
2024-03-15 15:14:27 -0300

    feat: create function to find Veiculo by Id

--- src/main/kotlin/com/francisco/service/VeiculoService.kt
+++ src/main/kotlin/com/francisco/service/VeiculoService.kt
@@ -8,7 +8,12 @@ import jakarta.inject.Singleton
 class VeiculoService(
     private val veiculoRepository: VeiculoRepository
 ) {
+
     fun create(veiculo: Veiculo): Veiculo{
         return veiculoRepository.save(veiculo);
+    }
+
+    fun findById(id: Long): Veiculo{
+        return veiculoRepository.findById(id).get();
     }
 }
